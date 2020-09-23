252058070e8088bfdfee55057807d6ce3be71094
Romain LOUVET <romain.louvet@adevinta.com>
2020-09-23 16:32:02 +0200

    fix: close print message from api

--- js/messages.js
+++ js/messages.js
@@ -1,35 +1,35 @@
-$.getJSON("http://api-tivoly.duckdns.org:8080/messages", function (data, status) {
-    var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-
-    var i = 0;
-    $.each(data, function (index, value) {
-        var date = new Date(value.date);
-
-        // les noms de jours / mois
-        var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi",
-                              "samedi");
-        var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet",
-                             "aout", "septembre", "octobre", "novembre", "decembre");
-
-        var month = date.getMonth()+1;
-        // on recupere la date
-        // on construit le message
-        // var message = jours[date.getDay()] + " ";   // nom du jour
-        var message = date.getDate() + "/" + month;
-
-
-        $("#by-" + index).html("" + value.admin);
-        $("#posted_date-" + index).html("le " + message);
-        $("#text-" + index).html(value.text);
-        i++;
-    });
-
-    for (j = i; j < 10; j++) {
-        // do something
-        $("#box-" + j).html("");
-    }
-
-});
+// $.getJSON("http://api-tivoly.duckdns.org:8080/messages", function (data, status) {
+//     var options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+//
+//     var i = 0;
+//     $.each(data, function (index, value) {
+//         var date = new Date(value.date);
+//
+//         // les noms de jours / mois
+//         var jours = new Array("dimanche", "lundi", "mardi", "mercredi", "jeudi", "vendredi",
+//                               "samedi");
+//         var mois = new Array("janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet",
+//                              "aout", "septembre", "octobre", "novembre", "decembre");
+//
+//         var month = date.getMonth()+1;
+//         // on recupere la date
+//         // on construit le message
+//         // var message = jours[date.getDay()] + " ";   // nom du jour
+//         var message = date.getDate() + "/" + month;
+//
+//
+//         $("#by-" + index).html("" + value.admin);
+//         $("#posted_date-" + index).html("le " + message);
+//         $("#text-" + index).html(value.text);
+//         i++;
+//     });
+//
+//     for (j = i; j < 10; j++) {
+//         // do something
+//         $("#box-" + j).html("");
+//     }
+//
+// });
 
 $("#envoyer").click(function () {
     console.log("name:" + $("#name").val());
